b8f009eb95b9f59290ed954c0114abae74369613
Shahbaz Naeem <mail.shahbaznaeem1@gmail.com>
2023-01-18 01:50:53 +0500

    Configured User authentication API using Devise Token Auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'api/v1'
+ 
   root 'homepage#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
